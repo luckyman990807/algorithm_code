967b05784644e03c5b0e6bb9e132b3f9bee42550
Luckyman <luckyman990807@gmail.com>
2026-05-08 10:09:56 +0800

    打气球问题；多米诺与托米诺贴瓷砖问题

--- restart_20250810/src/dynamic_programing/SimplifyOutDependency/Class030ShootBalloons.java
+++ restart_20250810/src/dynamic_programing/SimplifyOutDependency/Class030ShootBalloons.java
@@ -2,6 +2,7 @@ package dynamic_programing.SimplifyOutDependency;
 
 /**
  * 打气球问题
+ * https://leetcode.cn/problems/burst-balloons/submissions/723459492/
  * 给定一个数组arr，代表一排有分数的气球，打爆一个气球的得分=当前气球分数*左边最近的没爆的气球的分数*右边最近的没爆的气球的分数，如果左边已经没有没爆的气球了，那么*1，右边同理。
  * 求最大得分
  * 
@@ -23,5 +24,84 @@ package dynamic_programing.SimplifyOutDependency;
  * 3、最后打爆i计算得分：helpArr[i]*helpArr[l-1]*helpArr[r+1]，为什么要*helpArr[l-1]，因为i是l到r范围最后打爆的，打i的时候l到r已经没有其他没爆的了，左边最近的没爆的就是l-1，这是潜台词。右边也同理。
  */
 public class Class030ShootBalloons {
+    /**
+     * 阶段一解法：暴力递归法
+     * @param nums
+     * @return
+     */
+    public static int force(int[] nums) {
+        // 辅助数组，左右两边各插一个1
+        int[] help = new int[nums.length + 2];
+        help[0] = 1;
+        help[help.length - 1] = 1;
+        for (int i = 0; i < nums.length; i++) {
+            help[i + 1] = nums[i];
+        }
 
+        // 递归，辅助数组0和n+1位置没爆，要把1到n都打爆，返回最大得分
+        return process(help, 1, nums.length);
+    }
+
+    /**
+     * 递归函数
+     * 从l到r的气球全部打爆，返回最大得分。默认潜台词：l-1和r+1位置都还没爆
+     * @param arr
+     * @param l
+     * @param r
+     * @return
+     */
+    private static int process(int[] arr, int l, int r) {
+        // 递归出口
+        // 如果从l到r只有一个气球，那么直接计算得分
+        if (l == r) {
+            return arr[l] * arr[l - 1] * arr[r + 1];
+        }
+        // 如果是无效区域，返回得分0
+        if (l > r) {
+            return 0;
+        }
+
+        // 枚举每个位置作为最后一个被打爆的可能性，选取得分最大的可能性
+        int max = Integer.MIN_VALUE;
+        for (int i = l; i <= r; i++) {
+            // i位置作为最后一个被打爆的，默认潜规则：l-1和r+1都没打爆，那么这种可能性的得分是：打爆l到i-1的得分 + 打爆i+1到r的得分 + 打爆i的得分
+            max = Math.max(max, process(arr, l, i - 1) + process(arr, i + 1, r) + arr[i] * arr[l - 1] * arr[r + 1]);
+        }
+
+        return max;
+    }
+
+    /**
+     * 第二阶段解法：完全位置依赖的动态规划
+     * 按照暴力递归法改写即可
+     * @param nums
+     * @return
+     */
+    public static int dp(int[] nums) {
+        // 辅助数组
+        int[] help = new int[nums.length + 2];
+        help[0] = 1;
+        help[help.length - 1] = 1;
+        for (int i = 0; i < nums.length; i++) {
+            help[i + 1] = nums[i];
+        }
+
+        // dp表，按照递归出口对dp表初始化
+        int[][] dp = new int[nums.length + 2][nums.length + 2];
+        for (int i = 1; i <= nums.length; i++) {
+            dp[i][i] = help[i] * help[i - 1] * help[i + 1];
+        }
+
+        // 分析依赖关系，依赖左侧格子和下方格子，因此l从大到小遍历，r从小到大遍历
+        for (int l = nums.length; l > 0; l--) {
+            for (int r = l; r <= nums.length; r++) {
+                // 按照递归逻辑改写状态转移方程
+                for (int i = l; i <= r; i++) {
+                    dp[l][r] = Math.max(dp[l][r], dp[l][i - 1] + dp[i + 1][r] + help[i] * help[l - 1] * help[r + 1]);
+                }
+            }
+        }
+
+        return dp[1][nums.length];
+    }
 }
